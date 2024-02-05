024a7d839e6d029752c734cd61f06b212a28d447
wilylansapp <71969380+wilylansapp@users.noreply.github.com>
2024-02-05 16:26:17 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-13-reactive-form-validation-test
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/wilylansapp/angular-13-reactive-form-validation-test)
---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, VERSION } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import Validation from './utils/validation';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {}
+}
